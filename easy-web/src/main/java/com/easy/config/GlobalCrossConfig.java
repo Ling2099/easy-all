2ae898c5c71c61a82ab6dc61e002ff7a26d6fa42
Ling2099 <13452110075@163.com>
2024-06-01 08:34:08 +0800

    feat: 增加springboot2.7.11跨域设置

--- easy-web/src/main/java/com/easy/config/GlobalCrossConfig.java
+++ easy-web/src/main/java/com/easy/config/GlobalCrossConfig.java
@@ -5,7 +5,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.reactive.CorsWebFilter;
 import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 import org.springframework.web.util.pattern.PathPatternParser;
+
+import java.util.Collections;
 
 /**
  * 全局跨于设置
@@ -14,15 +17,17 @@ import org.springframework.web.util.pattern.PathPatternParser;
  * @version 1.0.9
  * @since 2023/07/21
  */
-@ConditionalOnProperty(name = "easy.cross")
 public class GlobalCrossConfig {
 
     /**
-     * 配置跨域
+     * 跨域配置
+     *
+     * <p>reactive 版本</p>
      *
      * @return {@link CorsWebFilter}
      */
     @Bean
+    @ConditionalOnProperty(name = "easy.cross-web")
     public CorsWebFilter crossFilter() {
         CorsConfiguration config = new CorsConfiguration();
         config.addAllowedMethod("*");
@@ -33,6 +38,27 @@ public class GlobalCrossConfig {
         source.registerCorsConfiguration("/**", config);
 
         return new CorsWebFilter(source);
+    }
+
+    /**
+     * 跨域配置
+     *
+     * <p>SpringBoot 2.7.11 版本</p>
+     *
+     * @return {@link CorsFilter}
+     * @since 1.0.12
+     */
+    @Bean
+    @ConditionalOnProperty(name = "easy.cross")
+    public CorsFilter corsFilter() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.setAllowedOriginPatterns(Collections.singletonList("*"));
+        corsConfiguration.addAllowedHeader(CorsConfiguration.ALL);
+        corsConfiguration.addAllowedMethod(CorsConfiguration.ALL);
+        corsConfiguration.setAllowCredentials(true);
+        org.springframework.web.cors.UrlBasedCorsConfigurationSource source = new org.springframework.web.cors.UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfiguration);
+        return new CorsFilter(source);
     }
 
 }
